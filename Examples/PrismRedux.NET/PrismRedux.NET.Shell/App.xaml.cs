0655576c4654cf47a98b675c931983f31c3f633c
fswadling <fswadling@gmail.com>
2017-09-02 15:28:30 +0100

    Added PrismRedux project and shell project

---
+++ Examples/PrismRedux.NET/PrismRedux.NET.Shell/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrismRedux.NET.Shell
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            BootStrapper bootstrapper = new BootStrapper();
+            bootstrapper.Run();
+        }
+    }
+}
---
+++ Examples/PrismRedux.NET/PrismRedux.NET.Shell/BootStrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Mef;
+using Prism.Modularity;
+
+namespace PrismRedux.NET.Shell
+{
+    public class BootStrapper : MefBootstrapper
+    {
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new DirectoryModuleCatalog() { ModulePath = "..\\..\\..\\Modules" };
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return new PrismRedux.NET.Shell.Shell();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (PrismRedux.NET.Shell.Shell)Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
